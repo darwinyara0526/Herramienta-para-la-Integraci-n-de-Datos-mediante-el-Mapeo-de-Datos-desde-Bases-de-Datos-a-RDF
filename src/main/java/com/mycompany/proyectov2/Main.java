001c5cead78d4fde5394ef5d6a4228bf07e8b6ab
darwin <darwin@darwin>
2024-10-15 23:54:39 -0500

    rdf generado se ve desosganizado - para pruebas v1

--- src/main/java/com/mycompany/proyectov2/Main.java
+++ src/main/java/com/mycompany/proyectov2/Main.java
@@ -1,6 +1,7 @@
 package com.mycompany.proyectov2;
 
 import com.mycompany.filegeneration.MySQLR2RMLGenerator;
+import com.mycompany.filegeneration.MySQLRDFConverter;
 import com.mycompany.filegeneration.MySQLDataReader;
 import com.mycompany.database.DatabaseConnectionMySQL;
 
@@ -8,8 +9,9 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        // Ruta donde se guardarán los archivos R2RML y RDF
+        // Rutas donde se guardarán los archivos R2RML y RDF
         String outputPath = "/home/darwin/Escritorio/Proyecto/archivo.ttl"; // Archivo Turtle
+        String outputRDFPath = "/home/darwin/Escritorio/Proyecto/archivo.rdf"; // Archivo RDF/XML
 
         // Crear instancia de DatabaseConnectionMySQL
         DatabaseConnectionMySQL dbConnection = new DatabaseConnectionMySQL();
@@ -23,11 +25,16 @@ public class Main {
             MySQLR2RMLGenerator r2rmlGenerator = new MySQLR2RMLGenerator(connection);
 
             // Leer y mostrar datos de las tablas
-            MySQLDataReader dataReader = new MySQLDataReader(connection); // Ahora funciona correctamente
+            MySQLDataReader dataReader = new MySQLDataReader(connection);
             dataReader.readTables();
 
             // Generar el archivo R2RML
             r2rmlGenerator.generateR2RML(outputPath);
+            
+            // Crear instancia del convertidor RDF
+            MySQLRDFConverter rdfConverter = new MySQLRDFConverter(connection);
+            // Convertir el archivo Turtle a RDF/XML
+            rdfConverter.convertToRDF(outputPath, outputRDFPath);
         } catch (Exception e) {
             e.printStackTrace(); // Manejo de excepciones
         } finally {
